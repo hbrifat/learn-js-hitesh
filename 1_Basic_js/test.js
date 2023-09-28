d3953ae6ec1e88e7b071ef5fe20626a8c20e9dc9
hbrifat <hbrifat@gmail.com>
2023-09-28 21:13:10 +0600

    some changes in test.js

--- 1_Basic_js/test.js
+++ 1_Basic_js/test.js
@@ -9,3 +9,15 @@ accountAddress = "bd"
 let accountState
 
 console.table([accountName, piValue, accountAddress, accountState, accountUser])
+
+
+
+//alert("hello!") // it's node; not browser!
+
+
+let isLogggedIn = " R"
+let isLogggedInToBool = Boolean(isLogggedIn)
+console.log(isLogggedIn, isLogggedInToBool)
+console.log(isLogggedIn)
+console.log(isLogggedInToBool);
+console.log(typeof isLogggedInToBool);
